96a11878612e76e02140f15c1847b93f0a47d7f7
annie0711 <annie0711@gmail.com>
2019-09-22 16:01:15 +0800

    update

--- Program.cs
+++ Program.cs
@@ -6,22 +6,25 @@ using System.Threading.Tasks;
 using InterfaceDemo.DataClass;
 using abstractDemo.DataClass;
 using delegateDemo.DataClass;
+using FuncDemo.DataClass;
 namespace InterfaceDemo
 {
 	public class Program
 	{
-		
+
 		static void Main(string[] args)
 		{
 			//interfaceDemo();
 			//abstractDemo();
-			delegateDemo();
+			//delegateDemo();
+			funcDemo();
 			Console.Read();
 		}
 		/// <summary>
 		/// 介面
 		/// </summary>
-		static void interfaceDemo() {
+		static void interfaceDemo()
+		{
 			Car car = new Car();
 			Console.WriteLine(car.Name);
 			car.Run();
@@ -36,10 +39,41 @@ namespace InterfaceDemo
 			taiwan.Name = "臺灣人";
 			taiwan.SayHello();
 		}
-		static void delegateDemo() {
+		static void delegateDemo()
+		{
 			Custom custom = new Custom(SendAction.SendFromMon);
 			Console.Write($"媽媽{custom(10).ToString()}");
 		}
-		
+
+		static void funcDemo()
+		{
+			//Init一個Person物件
+			Person p = new Person()
+			{
+				Age = 10,
+				Name = "tom"
+			};
+			#region Func
+			//宣告一個Func<Person,string>委託 _thunkCheckAge
+			Func<Person, string> _thunkCheckAge;
+			//_thunkCheckAge委託指向CheckAge方法
+			_thunkCheckAge = new Func<Person, string>(CheckAge);
+			//執行_thunkCheckAge委託 (執行CheckAge方法)
+			string result = _thunkCheckAge(p);
+			//最後將結果顯示出來
+			Console.WriteLine(result);
+			#endregion
+			Console.ReadKey();
+		}
+		public static string CheckAge(Person person)
+		{
+			string result = "年紀剛剛好";
+			if (person.Age >= 10)
+			{
+				result = "老人";
+			}
+			return result;
+		}
+
 	}
 }
